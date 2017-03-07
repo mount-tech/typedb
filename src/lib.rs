eeb2007a12b66a9c05d9cb2fcf4d30fef3a5db43
shockham <samuel.hockham@gmail.com>
2017-03-07 09:47:58 +0000

    Added top level crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,54 @@
+/*!
+Simple persistent generic HashMap/Key-value store.
+
+Basic usage:
+
+```
+extern crate kv_cab;
+
+use kv_cab::{ KV, Value };
+
+fn main() {
+    let mut test_store = KV::<String, Value>::new("./db.cab");
+
+    let _ = test_store.insert("key".to_string(), Value::String("value".to_string()));
+    println!("{:?}", test_store.get("key".to_string()));
+    let _ = test_store.remove("key".to_string());
+}
+```
+
+Usage with user defined Key and Value types:
+
+```
+extern crate kv_cab;
+extern crate rustc_serialize;
+
+use kv_cab::KV;
+
+#[derive(Clone, RustcEncodable, RustcDecodable, PartialEq, Eq, Hash)]
+enum MyKey {
+    String(String),
+    Int(i32),
+}
+
+#[derive(Clone, RustcEncodable, RustcDecodable, Debug)]
+enum MyValue {
+    String(String),
+    Int(i32),
+}
+
+fn main() {
+    let mut test_store = KV::<MyKey, MyValue>::new("./db.cab");
+
+    let _ = test_store.insert(MyKey::Int(1i32), MyValue::String("value".to_string()));
+    println!("{:?}", test_store.get(MyKey::Int(1i32)));
+    let _ = test_store.remove(MyKey::Int(1i32));
+}
+```
+
+*/
+
+
 extern crate bincode;
 extern crate rustc_serialize;
 #[macro_use]
