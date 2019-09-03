8a60bb52eaaec483164c8999551da5be24640145
Erik Zscheile <erik.zscheile@gmail.com>
2019-09-03 16:30:12 +0200

    Add benchmark for insert+get

--- benches/benches.rs
+++ benches/benches.rs
@@ -45,4 +45,18 @@ mod benches {
 
         bench_teardown!(test_cab_path);
     }
+
+    #[bench]
+    fn bench_insert_get_int(b: &mut Bencher) {
+        let test_cab_path = "./bench_insert_get_many.cab";
+        let mut test_store = KV::<String, Value>::new(test_cab_path).unwrap();
+        let test_as_string = "test".to_string();
+
+        b.iter(|| {
+            let _ = test_store.insert(test_as_string.clone(), Value::Int(1));
+            let _ = test_store.get(&test_as_string);
+        });
+
+        bench_teardown!(test_cab_path);
+    }
 }
